ce345e6b9d27e5b81dd895e1c08e8fd2271fa5cd
Mason Llewellyn <masonllewellyn@fb.com>
2020-06-25 18:53:12 -0400

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Mason Llewellyn on 6/25/20.
+//  Copyright © 2020 Facebook University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
